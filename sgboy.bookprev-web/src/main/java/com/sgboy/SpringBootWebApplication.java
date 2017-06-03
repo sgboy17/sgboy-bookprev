435edb3017cde95a69c7cd8a21d2526f9e8054d1
sgboy17 <nghiapv485@gmail.com>
2017-06-03 13:36:56 +0700

    added Thymeleaf sample

---
+++ sgboy.bookprev-web/src/main/java/com/sgboy/SpringBootWebApplication.java
@@ -0,0 +1,23 @@
+package com.sgboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by sgboy17 on 5/18/2017.
+ */
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
